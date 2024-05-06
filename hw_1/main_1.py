3c5cf0c6f2a0abf7067f8ffcb2649a14f0dcd598
nanashi <egorglazkov71@gmail.com>
2024-05-06 18:55:29 +0300

    implemented the methods of the class animal

--- hw_1/main_1.py
+++ hw_1/main_1.py
@@ -1,9 +1,13 @@
 class Animal:
-    def __init__(self, ):
-        pass
+    def __init__(self, name: str, age: int, sound: str):
+        self.name = name
+        self.age = age
+        self.sound = sound
 
     def play_sound(self):
-        pass
+        print(f"Animal scream: {self.sound}")
 
     def print_data(self):
-        pass
+        print(f"Animal's name: {self.name}")
+        print(f"Animal's age: {self.age}")
+        print(f"Animal's sound: {self.sound}")
