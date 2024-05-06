15272b901982611ce94d460f41b6b6414605178b
nanashi <egorglazkov71@gmail.com>
2024-05-06 19:55:21 +0300

    add output result work classes

--- hw_1/main_1.py
+++ hw_1/main_1.py
@@ -1,4 +1,4 @@
-#Task 1
+# Task 1
 class Animal:
     def __init__(self, name: str, age: int, sound: str):
         self.name = name
@@ -14,7 +14,7 @@ class Animal:
         print(f"Animal's sound: {self.sound}")
 
 
-#Task 2
+# Task 2
 class Book:
     def __init__(self, name: str, author: str, count_page: int):
         self.name = name
@@ -24,18 +24,18 @@ class Book:
     def open_page(self, page: int):
         if page > self.count_page:
             print("ERROR! Count page is so large!")
-        elif page <0:
+        elif page < 0:
             print("ERROR! Page is so small!")
         else:
             print(f"Opening page: {page}")
 
-    def print_date(self):
+    def print_data(self):
         print(f"Name: {self.name}")
         print(f"Author: {self.author}")
         print(f"Count page: {self.count_page}")
 
 
-#Task 3
+# Task 3
 class PassengerPlane:
     def __init__(self, maker: str, model: str, passenger_capacity: int, current_height: int, current_velocity: int):
         self.maker = maker
@@ -70,7 +70,7 @@ class PassengerPlane:
         print(f"Current velocity: {self.current_velocity}")
 
 
-#Task 4
+# Task 4
 class MusicAlbum:
     def __init__(self, artist: str, album_name: str, genre: str, track_list: []):
         self.artist = artist
@@ -78,14 +78,18 @@ class MusicAlbum:
         self.genre = genre
         self.track_list = track_list
 
-    def add_song_in_album(self, track_name: str):
-        self.track_list.append(track_name)
+    def add_songs_in_album(self, tracks_names: []):
+        for k in range(0, len(tracks_names)):
+            self.track_list.append(tracks_names[k])
+            print(f"In album adding: {tracks_names[k]}")
 
-    def remove_song_in_album(self, track_name: str):
-        self.track_list.remove(track_name)
+    def remove_song_in_album(self, tracks_names: []):
+        for k in range(0, len(tracks_names)):
+            self.track_list.remove(tracks_names[k])
+            print(f"In album remove: {tracks_names[k]}")
 
     def play_song_in_album(self, track_name: str):
-        if self.track_list.Count(track_name) > 0:
+        if self.track_list.count(track_name) > 0:
             print(f"Play {track_name}")
         else:
             print(f"ERROR! Track {track_name} doesn't find!")
@@ -95,3 +99,38 @@ class MusicAlbum:
         print(f"Album: {self.album_name}")
         print(f"Genre: {self.genre}")
         print(f"Track list: {self.track_list}")
+
+
+class Program:
+    @staticmethod
+    def main():
+        print("---Task_1---")
+        a1 = Animal("Cat Boris", 2, "MEOW MEOW")
+        a1.play_sound()
+        a1.print_data()
+
+        print("\n ---Task_2---")
+        book1 = Book("Crime and punishment", "Dostoevsky", 672)
+        book1.open_page(1232)
+        book1.open_page(523)
+        book1.print_data()
+
+        print("\n ---Task_3---")
+        passenger_plane1 = PassengerPlane("some famous builder", "ARC100 FC72", 222, 100, 300)
+        passenger_plane1.take_off_plane()
+        passenger_plane1.change_height(-100)
+        passenger_plane1.change_height(2500)
+        passenger_plane1.change_velocity(500)
+        passenger_plane1.landing_plane()
+        passenger_plane1.print_data()
+
+        print("\n ---Task_4---")
+        album1 = MusicAlbum("NEWLIGHTCHILD", "NIGHT FOR EVER", "electro rok", [])
+        album1.add_songs_in_album(["GESHTALT", "VIP", "VOYY NA LUNU", "AMORE", "MONEY DANCE"])
+        album1.remove_song_in_album(["AMORE", "MONEY DANCE"])
+        album1.play_song_in_album("AMORE")
+        album1.play_song_in_album("VIP")
+        album1.print_data()
+
+
+Program.main()
